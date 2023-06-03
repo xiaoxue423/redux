9fa251e9784779e3480ea8c7409761bc54db1ca8
xiaoxue <xiaoxuexue19980423@163.com>
2023-06-03 17:57:53 +0800

    把store一次性传给应用里的所有容器组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Count from "./containers/Count"; //引入容器组件
-import store from "./redux/store";
 
 function App() {
   return (
     <div>
       {/* 渲染容器组件 给容器组件传递store */}
-      <Count store={store} />
+      <Count />
     </div>
   );
 }
